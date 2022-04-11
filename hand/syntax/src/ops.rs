d938c826998aa829a1bf2fc314771b8e27f38fef
Joe Loach <joe.gloach@gmail.com>
2022-04-11 15:52:59 +0100

    add doc comments for each opcode

--- hand/syntax/src/ops.rs
+++ hand/syntax/src/ops.rs
@@ -1,93 +1,93 @@
 opcodes! {
-    ADC     ; // Add with carry
+    ADC     ; /// Add with carry
     ADCS    ;
-    ADD     ; // Add
+    ADD     ; /// Add
     ADDS    ;
-    ADR     ; // Address of
-    AND     ; // Bitwise AND
-    ASR     ; // Arithmetic Shift Right
+    ADR     ; /// Address of
+    AND     ; /// Bitwise AND
+    ASR     ; /// Arithmetic Shift Right
     ASRS    ;
-    B       ; // Branch to target address
-    BFC     ; // Bit Field Clear
-    BFI     ; // Bit Field Insert
-    BIC     ; // Bitwise Bit Clear
+    B       ; /// Branch to target address
+    BFC     ; /// Bit Field Clear
+    BFI     ; /// Bit Field Insert
+    BIC     ; /// Bitwise Bit Clear
     BICS    ;
-    BL      ; // Branch with Link
-    BX      ; // Branch and Exchange
-    CLZ     ; // Count Leading Zeros
-    CMN     ; // Compare Negative
-    CMP     ; // Compare
-    EOR     ; // Bitwise Exclusive OR
+    BL      ; /// Branch with Link
+    BX      ; /// Branch and Exchange
+    CLZ     ; /// Count Leading Zeros
+    CMN     ; /// Compare Negative
+    CMP     ; /// Compare
+    EOR     ; /// Bitwise Exclusive OR
     EORS    ;
-    LDA     ; // Load-Acquire Word
-    LDAB    ; // Load-Acquire Byte
-    LDAH    ; // Load-Acquire Halfword
-    LDM     ; // Load Multiple Increment After
-    LDMIA   ; // Load Multiple Increment After
-    LDMFD   ; // Load Multiple Full Descending
-    LDMDA   ; // Load Multiple Decrement After
-    LDMFA   ; // Load Multiple Full Ascending
-    LDMDB   ; // Load Multiple Decrement Before
-    LDMEA   ; // Load Multiple Empty Ascending
-    LDMIB   ; // Load Multiple Increment Before
-    LDMED   ; // Load Multiple Empty Descending
-    LDR     ; // Load Register
-    LDRB    ; // Load Register Byte
-    LDRD    ; // Load Register Dual
-    LDRH    ; // Load Register Halfword
-    LDRSB   ; // Load Register Signed Byte
-    LDRSH   ; // Load Register Signed Halfword
-    LSL     ; // Logical Shift Left
+    LDA     ; /// Load-Acquire Word
+    LDAB    ; /// Load-Acquire Byte
+    LDAH    ; /// Load-Acquire Halfword
+    LDM     ; /// Load Multiple Increment After
+    LDMIA   ; /// Load Multiple Increment After
+    LDMFD   ; /// Load Multiple Full Descending
+    LDMDA   ; /// Load Multiple Decrement After
+    LDMFA   ; /// Load Multiple Full Ascending
+    LDMDB   ; /// Load Multiple Decrement Before
+    LDMEA   ; /// Load Multiple Empty Ascending
+    LDMIB   ; /// Load Multiple Increment Before
+    LDMED   ; /// Load Multiple Empty Descending
+    LDR     ; /// Load Register
+    LDRB    ; /// Load Register Byte
+    LDRD    ; /// Load Register Dual
+    LDRH    ; /// Load Register Halfword
+    LDRSB   ; /// Load Register Signed Byte
+    LDRSH   ; /// Load Register Signed Halfword
+    LSL     ; /// Logical Shift Left
     LSLS    ;
-    LSR     ; // Logical Shift Right
-    LSRS    ; // Logical Shift Right
-    MLA     ; // Multiply Accumulate
+    LSR     ; /// Logical Shift Right
+    LSRS    ; /// Logical Shift Right
+    MLA     ; /// Multiply Accumulate
     MLAS    ;
-    MLS     ; // Multiply and Subtract
-    MOV     ; // Move
+    MLS     ; /// Multiply and Subtract
+    MOV     ; /// Move
     MOVS    ;
-    MOVT    ; // Move Top
-    MUL     ; // Multiply
+    MOVT    ; /// Move Top
+    MUL     ; /// Multiply
     MULS    ;
-    MVN     ; // Bitwise NOT
+    MVN     ; /// Bitwise NOT
     MVNS    ;
-    NOP     ; (args = false) // No Operation
-    ORR     ; // Bitwise OR
+    NOP     ; (args = false) /// No Operation
+    ORR     ; /// Bitwise OR
     OORS    ;
-    POP     ; // Pop Multiple Registers from Stack
-    PUSH    ; // Push Mutliple Registers to Stack
-    RBIT    ; // Reverse Bits
-    REV     ; // Byte-Reverse Word
-    ROR     ; // Rotate Right
+    POP     ; /// Pop Multiple Registers from Stack
+    PUSH    ; /// Push Mutliple Registers to Stack
+    RBIT    ; /// Reverse Bits
+    REV     ; /// Byte-Reverse Word
+    ROR     ; /// Rotate Right
     RORS    ;
-    RRX     ; // Rotate Right with Extend
+    RRX     ; /// Rotate Right with Extend
     RRXS    ;
-    RSB     ; // Reverse Subtract
+    RSB     ; /// Reverse Subtract
     RSBS    ;
-    RSC     ; // Reverse Subtract with Carry
+    RSC     ; /// Reverse Subtract with Carry
     RSCS    ;
-    SBC     ; // Subtract with Carry
+    SBC     ; /// Subtract with Carry
     SBCS    ;
-    SDIV    ; // Signed Divide
-    STM     ; // Store Mutliple
-    STMIA   ; // Store Mutliple Increment After
-    STMEA   ; // Store Mutliple Empty Ascending
-    STMDA   ; // Store Multiple Decrement After
-    STMED   ; // Store Multiple Empty Descending
-    STMDB   ; // Store Multiple Decrement Before
-    STMFD   ; // Store Multiple Full Descending
-    STMIB   ; // Store Mutliple Increment Before
-    STMFA   ; // Store Mutliple Full Ascending
-    STR     ; // Store Register
-    STRB    ; // Store Register Byte
-    STRD    ; // Store Register Dual
-    STRH    ; // Store Register Halfword
-    SUB     ; // Subtract
+    SDIV    ; /// Signed Divide
+    STM     ; /// Store Mutliple
+    STMIA   ; /// Store Mutliple Increment After
+    STMEA   ; /// Store Mutliple Empty Ascending
+    STMDA   ; /// Store Multiple Decrement After
+    STMED   ; /// Store Multiple Empty Descending
+    STMDB   ; /// Store Multiple Decrement Before
+    STMFD   ; /// Store Multiple Full Descending
+    STMIB   ; /// Store Mutliple Increment Before
+    STMFA   ; /// Store Mutliple Full Ascending
+    STR     ; /// Store Register
+    STRB    ; /// Store Register Byte
+    STRD    ; /// Store Register Dual
+    STRH    ; /// Store Register Halfword
+    SUB     ; /// Subtract
     SUBS    ;
-    SVC     ; // Supervisor Call
-    TEQ     ; // Test Equivalence (Bitwise XOR)
-    TST     ; // Test (Bitwise AND)
-    UDIV    ; // Unsigned Divide
+    SVC     ; /// Supervisor Call
+    TEQ     ; /// Test Equivalence (Bitwise XOR)
+    TST     ; /// Test (Bitwise AND)
+    UDIV    ; /// Unsigned Divide
 }
 
 impl core::str::FromStr for Opcode {
@@ -106,8 +106,7 @@ impl core::str::FromStr for Opcode {
 macro_rules! opcodes {
     (
         $(
-            $(#[$meta:meta])*
-            $opid:ident; $((args = $args:expr))?
+            $opid:ident; $((args = $args:expr))? $(#[$meta:meta])*
         )*
         $(;)?
     ) => {
